a09ee84fed7816c2d60590da7887111ca786d68b
Pierre Nowak <pnowak@eudonet.com>
2020-11-19 14:08:12 +0100

    changement d'api pour wikitionnary, plus rapide, plus puissante plus conséquente

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
 <template>
   <v-app>
-    <v-container grid-list-md class="center">
-      <v-list class="text-center">
-        <v-list-item v-for="word in wordList" :key="word.id">
-          {{ word }}
-        </v-list-item>
-      </v-list>
-      <div class="CaBFont">{{ wordPlayed }}</div>
-      <v-spacer></v-spacer>
-      <v-text-field class="w-80" v-model="wordInput" @keypress.enter="addWord(wordInput)"> </v-text-field>
+    <v-container class="fill-height">
+      <v-row cols="12" class="baseContent">
+        <v-col cols="3">
+          <v-list>
+            <v-list-item v-for="word in wordList" :key="word.id">
+              {{ word }}
+            </v-list-item>
+          </v-list></v-col
+        >
+
+        <v-col cols="6" align-self="center">
+          <div class="CaBFont text-center">{{ wordPlayed }}</div>
+        </v-col>
+        <v-col cols="3"></v-col>
+      </v-row>
+      <v-row cols="12">
+        <v-col cols="3" class="text-center text--accent-2" align-self="center">
+          <h2>Joueur</h2>
+          <h3>{{ playerPoints }}</h3></v-col
+        >
+        <v-col cols="6">
+          <v-text-field
+            color="#ED531D"
+            outlined
+            v-model="wordInput"
+            @keypress.enter="addWord(wordInput)"
+            append-icon="mdi-send"
+          >
+          </v-text-field>
+        </v-col>
+        <v-col cols="3" class="text-center text--accent-1" align-self="center">
+          <h2>Ordinateur</h2>
+
+          <h3>{{ comPoints }}</h3></v-col
+        >
+      </v-row>
     </v-container>
   </v-app>
 </template>
@@ -22,6 +49,11 @@ export default {
   components: {},
 
   data: () => ({
+    //Pattern de comptage
+    letters: 'abcdefghijklmnopqrstuvwxyz',
+    objectLetters: require('./assets/scrabble.json').letters,
+
+    //Variables de jeu
     wordInput: '',
     wordList: [],
     textTyping: '',
@@ -41,7 +73,28 @@ export default {
     //Audios
     keyStroke: new Audio(require('./assets/keyStroke.mp3')),
   }),
+  watch: {
+    calcPoints() {
+      console.log('calc points = ' + this.calcPoints)
+    },
+  },
   methods: {
+    totalLetters(word) {
+      let totalPoints = 0
+      for (let i = 0; i < word.length; i++) {
+        const element = word[i]
+        this.objectLetters[element].points
+        totalPoints += this.objectLetters[element].points
+      }
+      return totalPoints
+    },
+    howManyLettersBetween(word1, word2) {
+      if (this.letters.indexOf(word1[0]) - this.letters.indexOf(word2[0]) > 1) {
+        return this.letters.indexOf(word1[0]) - this.letters.indexOf(word2[0])
+      } else {
+        return this.letters.indexOf(word2[0]) - this.letters.indexOf(word1[0])
+      }
+    },
     getWord() {
       axios
         .get(
@@ -50,26 +103,29 @@ export default {
         )
         .then((response) => {
           this.isValidWord(response.data[0].mot)
+          console.log(response.data[0].mot)
           // let word = response.data[0].mot
           // this.textType(word)
         })
     },
     isValidWord(word) {
+      // debugger
       axios
-        .get('https://api.dicolink.com/v1/mot/' + word + '/scorescrabble?api_key=' + this.apiKey)
+        // .get('https://api.dicolink.com/v1/mot/' + word + '/definitions?limite=200&api_key=' + this.apiKey)
         // .get('https://api.dictionaryapi.dev/api/v2/entries/fr/' + word)
+        .get('https://fr.wiktionary.org/w/api.php?action=query&titles=' + word + '&format=json&origin=*')
+
         .then((response) => {
-          if (response.data.error !== undefined) {
+          console.log(response)
+          if (response.data.query.pages.hasOwnProperty('-1')) {
             // Le mot est inconnu
             console.log("Ce mot n'existe pas... tête de fion.")
           } else {
-            // Le mot est reconnu
+            // Le mot est reconnu on lance le script de typing
             this.textType(word)
-            console.log(response.data.score)
-            this.calcPoints = response.data.score
-
-            console.log(word)
-            // addWord(wordInput)
+            console.log(response.data)
+            // this.calcPoints += response.data.score
+            // console.log(this.calcPoints)
           }
         })
         .catch((error) => {
@@ -98,17 +154,18 @@ export default {
 
       if (this.index === 0) {
         this.refWord = this.wordInput
-        this.refWord = this.removeAccents(this.refWord)
+        // this.refWord = this.removeAccents(this.refWord)
       }
       this.wordInput = ''
 
       if (this.index < this.refWord.length) {
         this.isTyping = true
         console.log(this.index)
-        this.wordPlayed = this.wordPlayed + this.refWord.charAt(this.index)
+        this.wordPlayed = this.wordPlayed + this.removeAccents(this.refWord.charAt(this.index))
         this.index++
         this.keyStroke.play()
         this.isTyping = true
+
         setTimeout(this.typeWriter, this.randomDelay(300))
       } else if (this.index === this.refWord.length) {
         // Le typing du mot est terminé
@@ -118,6 +175,18 @@ export default {
         this.isTyping = false
         this.index = 0
         this.isTyping = false
+
+        //On ajoute les points de l'écart entre les premières lettres de chaque mots.
+        if (this.wordList.length > 1) {
+          this.calcPoints += this.howManyLettersBetween(this.wordList[0], this.wordList[1])
+          console.log('bonus hmlb + ' + this.calcPoints)
+        }
+
+        //Ajout du nombre de points scrabble
+        this.calcPoints += this.totalLetters(this.wordPlayed)
+        console.log(this.calcPoints)
+
+        console.log('points Scrabble = ' + this.totalLetters(this.wordPlayed))
 
         if (this.computerTurn === true) {
           this.computerTurn = false
@@ -133,11 +202,6 @@ export default {
       }
     },
     textType(word) {
-      // axios
-      //   .get('https://api.dicolink.com/v1/mot/' + word + '/scorescrabble?api_key=' + this.apiKey)
-      //   .then((response) => {
-      //     console.log(response.)
-      //   })
       this.wordPlayed = ''
       this.typeWriter(word)
     },
@@ -172,4 +236,7 @@ html
   .CaBFont
     font-family 'JD Scarabeo'
     font-size 60px
+
+  div.baseContent
+    height 80%
 </style>
